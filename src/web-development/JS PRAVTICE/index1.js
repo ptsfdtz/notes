88552646754df2f8ec74adf11f55b88a24fb461b
ptsfdtz <pitousanfadetuzi@gmail.com>
2023-12-30 17:12:09 +0800

    feat:add new js

---
+++ src/web-development/JS PRAVTICE/index1.js
@@ -0,0 +1,39 @@
+// //
+// document.write("adsdasdad")
+// document.write("</br>")
+// document.write("sdasdada")
+// //number
+1212
+3143
+
+//ture or false
+true;
+false;
+
+var myname = "PTSFDTZ";
+document.write(myname);
+document.write("</br>");
+
+var my_age = "19";
+document.write(my_age);
+document.write("</br>");
+var is_male = true;
+document.write(is_male);
+document.write("</br>");
+ 
+var phrase = "Hello world";
+document.write(phrase);
+document.write("</br>");
+document.write(phrase.toLowerCase());
+document.write("</br>");
+document.write(phrase.toUpperCase());
+document.write("</br>");
+
+document.write(phrase.charAt(0));
+document.write("</br>");
+
+document.write(phrase.indexOf("l"));
+document.write("</br>");
+
+document.write(phrase.substring(2,10));
+document.write("</br>");
